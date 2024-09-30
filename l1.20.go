29e482dd966be1dd2f67e209c87f97d2595c45de
Magomed Zakaryaev <zkryaev.dev@yandex.ru>
2024-09-30 11:01:43 +0300

    Solved l1.20

---
+++ l1.20.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	// считывание
+	reader := bufio.NewReader(os.Stdin)
+	text, _, _ := reader.ReadLine()
+
+	// разбиваем на слова
+	str := string(text)
+	strs := strings.Split(str, " ")
+
+	// меняем порядок
+	j := len(strs) - 1
+	for i := 0; i < len(strs)/2; i++ {
+		strs[i], strs[j] = strs[j], strs[i]
+	}
+
+	// конкатенируем
+	var result bytes.Buffer
+	for i := range strs {
+		result.WriteString(strs[i])
+		if i != len(strs)-1 {
+			result.WriteString(" ")
+		}
+	}
+	// результат
+	fmt.Printf("%s\n", result.String())
+}
